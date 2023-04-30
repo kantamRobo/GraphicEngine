e8994e34d5ed3927b9b31207581ee954b301c9c4
anazi6j <36531957+anazi6j@users.noreply.github.com>
2023-04-30 19:14:13 +0900

    Shaderを追加

--- Shader.h
+++ Shader.h
@@ -1,5 +1,47 @@
 #pragma once
+#include "stdafx.h"
 class Shader
 {
+	/// <summary>
+	/// ピクセルシェーダーをロード。
+	/// </summary>
+	/// <param name="filePath">ファイルパス。</param>
+	/// <param name="entryFuncName">エントリーポイントの関数名。</param>
+	void LoadPS(const char* filePath, const char* entryFuncName);
+	/// <summary>
+	/// 頂点シェーダーをロード。
+	/// </summary>
+	/// <param name="filePath">ファイルパス</param>
+	/// <param name="entryFuncName">エントリーポイントの関数名。</param>
+	void LoadVS(const char* filePath, const char* entryFuncName);
+	/// <summary>
+	/// コンピュートシェーダーをロード。
+	/// </summary>
+	/// <param name="filePath"></param>
+	/// <param name="entryFuncName"></param>
+	void LoadCS(const char* filePath, const char* entryFuncName);
+
+	ComPtr<ID3DBlob> GetCompiledBlob()const
+	{
+		return m_blob;
+	}
+	ComPtr<IDxcBlob> GetCompiledDxcBlob()
+	{
+		return m_dxcBlob;
+	}
+	/// <summary>
+	/// 初期化済み？
+	/// </summary>
+	/// <returns></returns>
+	bool IsInited() const
+	{
+		return m_isInited;
+	}
+private:
+	void LoadShader(const char* filePath, const char* entryFuncName, const char* shaderModel);
+private:
+	ComPtr<ID3DBlob> m_blob = nullptr;
+	ComPtr <IDxcBlob> m_dxcBlob = nullptr;
+	bool m_isInited = false;
 };
 
