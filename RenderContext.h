0ee6f06b03ce5e1b1697e77185f22c8d0eb51510
anazi6j <hattendosummer@yahoo.co.jp>
2023-05-02 19:02:06 +0900

    RenderContextの途中まで追加

--- RenderContext.h
+++ RenderContext.h
@@ -12,6 +12,147 @@ public:
 	{
 		m_commandList = commandList;
 	}
+	/// <summary>
+	/// 頂点バッファを設定。
+	/// </summary>
+	/// <param name="vb">頂点バッファ。</param>
+	void SetVertexBuffer(VertexBuffer& vb)
+	{
+		m_commandList->IASetVertexBuffers(0, 1, &vb.GetView());
+	}
+	/// <summary>
+	/// インデックスバッファを設定。
+	/// </summary>
+	/// <param name="ib"></param>
+	void SetIndexBuffer(IndexBuffer& ib)
+	{
+		m_commandList->IASetIndexBuffer(&ib.GetView());
+	}
+	/// <summary>
+	/// プリミティブのトポロジーを設定。
+	/// </summary>
+	/// <remarks>
+	/// ID3D12GraphicsCommandList::のIASetPrimitiveTopologyのラッパー関数。
+	/// 詳細はMicrosoftのヘルプを参照。
+	/// </remarks>
+	void SetPrimitiveTopology(D3D12_PRIMITIVE_TOPOLOGY topology)
+	{
+		m_commandList->IASetPrimitiveTopology(topology);
+	}
+	/// <summary>
+	/// コマンドリストを設定。
+	/// </summary>
+	/// <param name="commandList">コマンドリスト。</param>
+	void SetCommandList(ID3D12GraphicsCommandList4* commandList)
+	{
+		m_commandList = commandList;
+	}
+	/// <summary>
+	/// ビューポートとシザリング矩形をセットで設定
+	/// </summary>
+	/// <param name="viewport">ビューポート</param>
+	void SetViewportAndScissor(D3D12_VIEWPORT& viewport)
+	{
+		//シザリング矩形も設定する。
+		D3D12_RECT scissorRect;
+		scissorRect.bottom = static_cast<LONG>(viewport.Height);
+		scissorRect.top = 0;
+		scissorRect.left = 0;
+		scissorRect.right = static_cast<LONG>(viewport.Width);
+		SetScissorRect(scissorRect);
+
+		m_commandList->RSSetViewports(1, &viewport);
+		m_currentViewport = viewport;
+	}
+	/// <summary>
+	/// ビューポートを取得。
+	/// </summary>
+	/// <returns></returns>
+	D3D12_VIEWPORT GetViewport() const
+	{
+		return m_currentViewport;
+	}
+	/// <summary>
+	/// シザリング矩形を設定
+	/// </summary>
+	/// <param name="rect"></param>
+	void SetScissorRect(D3D12_RECT& rect)
+	{
+		m_commandList->RSSetScissorRects(1, &rect);
+	}
+
+	/// <summary>
+	/// ルートシグネチャを設定。
+	/// </summary>
+	void SetRootSignature(ID3D12RootSignature* rootSignature)
+	{
+		m_commandList->SetGraphicsRootSignature(rootSignature);
+	}
+	void SetRootSignature(RootSignature& rootSignature)
+	{
+		m_commandList->SetGraphicsRootSignature(rootSignature.Get());
+	}
+	void SetComputeRootSignature(ID3D12RootSignature* rootSignature)
+	{
+		m_commandList->SetComputeRootSignature(rootSignature);
+	}
+	void SetComputeRootSignature(RootSignature& rootSignature)
+	{
+		m_commandList->SetComputeRootSignature(rootSignature.Get());
+	}
+	/// <summary>
+	/// パイプラインステートを設定。
+	/// </summary>
+	void SetPipelineState(ID3D12PipelineState* pipelineState)
+	{
+		m_commandList->SetPipelineState(pipelineState);
+	}
+	void SetPipelineState(PipelineState& pipelineState)
+	{
+		m_commandList->SetPipelineState(pipelineState.Get());
+	}
+	/// <summary>
+	/// レイトレ用のパイプラインステートオブジェクトを設定。
+	/// </summary>
+	/// <param name="pso"></param>
+	void SetPipelineState(raytracing::PSO& pso);
+
+	/// <summary>
+	/// ディスクリプタヒープを設定。
+	/// </summary>
+	void SetDescriptorHeap(ID3D12DescriptorHeap* descHeap)
+	{
+		m_descriptorHeaps[0] = descHeap;
+		m_commandList->SetDescriptorHeaps(1, m_descriptorHeaps.data()->GetAddressOf());
+	}
+
+	void SetDescriptorHeap(DescriptorHeap& descHeap);
+	void SetComputeDescriptorHeap(DescriptorHeap& descHeap);
+
+	void SetDescriptorHeaps(int numDescriptorHeap, const ComPtr<DescriptorHeap> descHeaps[])
+	{
+		for (int i = 0; i < numDescriptorHeap; i++)
+		{
+			m_descriptorHeaps[i] = descHeaps[i];
+
+		}
+		m_commandList->SetDescriptorHeaps(numDescriptorHeap
+			, m_descriptorHeaps.data()->GetAddressOf());
+	}
+
+	void SetConstantBuffer(int registerNo, std::shared_ptr<ConstantBuffer> cb)
+	{
+		if (registerNo < MAX_CONSTANT_BUFFER) {
+			m_constantBuffers[registerNo] = cb;
+		}
+		else {
+			//範囲外アクセス。
+			std::abort();
+		}
+	}
+
+	void SetShaderResource(int registerNo, std::shared_ptr<Texture> texture)
+	{
 private:
 	enum { MAX_DESCRIPTOR_HEAP = 4 };
 	enum { MAX_CONSTANT_BUFFER = 8 };
