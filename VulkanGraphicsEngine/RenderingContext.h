eade3587490db310b78d01c8a96c6f4742b68d1e
Kanta yukawa <hattendosummer@yahoo.co.jp>
2023-06-07 11:35:27 +0900

    レンダリングコンテキスト,vulkan版進行中

--- VulkanGraphicsEngine/RenderingContext.h
+++ VulkanGraphicsEngine/RenderingContext.h
@@ -2,6 +2,22 @@
 class RenderingContext
 {//2023.6.7目標
 //４つ実装
-void InitRenderingContext()
+	void InitRenderingContext(std::shared_ptr<VkCommandBuffer> commandbuffer)
+	{
+		m_commandBuffer = commandbuffer;
+	}
+
+	//頂点バッファを設定
+	void SetVertexBuffer(std::shared_ptr<VertexBuffer> in_vb)
+	{
+		//TODO
+		vkCmdBindVertexBuffers(m_commandBuffer, );
+	}
+
+	//インデックスバッファを設定
+	void SetIndexBuffer(Index)
+
+private:
+	VkCommandBuffer m_commandBuffer;
 };
 
