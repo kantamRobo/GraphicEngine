ce7c0c31bc828d615dc6a5845f71d780e9c1c1bb
anazi6j <hattendosummer@yahoo.co.jp>
2023-06-20 16:16:41 +0900

    Vulkanエンジン、レンダリングコンテキスト、ある程度追加

--- VulkanGraphicsEngine/RenderingContext.h
+++ VulkanGraphicsEngine/RenderingContext.h
@@ -32,11 +32,46 @@ class RenderingContext
 
 	void SetViewportAndScissor(VkViewport& viewport)
 	{
-		VkRect2D scissorRect = {};
-		scissorRect.
-
+		VkRect2D rect = {};
+		rect.extent.width = viewport.width;
+		rect.extent.height = viewport.height;
+		rect.offset.x = 0;
+		rect.offset.y = 0;
+		
+		
+		m_currentViewport = viewport;
 	}
 
+	VkViewport GetViewport()
+	{
+		return m_currentViewport;
+	}
+
+	void SetGraphicPipelineState(VkPipeline pipeline)
+	{
+		vkCmdBindPipeline(m_commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, pipeline);
+	}
+
+	/*
+	void SetPipelineState(PipelineState& pipelineState)
+	{
+		m_commandList->SetPipelineState(pipelineState.Get());
+	}
+	
+	*/
+
+	/*
+	* 
+	* /// <summary>
+	/// レイトレ用のパイプラインステートオブジェクトを設定。
+	/// </summary>
+	/// <param name="pso"></param>
+	void SetPipelineState(raytracing::PSO& pso);
+	*/
+
+	
 private:
 	VkCommandBuffer m_commandBuffer;
+	VkPipeline m_pipeline;
+	VkViewport m_currentViewport;
 };
