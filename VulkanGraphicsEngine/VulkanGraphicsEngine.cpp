89bdecf1c8139227ba91afecf812615e78c1b1f1
anazi6j <36531957+anazi6j@users.noreply.github.com>
2023-06-24 03:31:01 +0900

    GraphicsEngineの途中まで

--- VulkanGraphicsEngine/VulkanGraphicsEngine.cpp
+++ VulkanGraphicsEngine/VulkanGraphicsEngine.cpp
@@ -1,3 +1,27 @@
 #include "VulkanGraphicsEngine.h"
 #include "stdafx.h"
 #include "RenderingContext.h"
+#include <GLFW/glfw3.h>
+bool VulkanGraphicsEngine::Init(int width,int height)
+{
+
+	g_graphicengine = this;
+	glfwInit();
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	glfwWindowHint(GLFW_RESIZABLE, 0);
+
+	m_frameBufferHeight = height;
+	m_frameBufferWidth = width;
+	auto window = glfwCreateWindow(m_frameBufferWidth, m_frameBufferHeight, "MainWindow", nullptr, nullptr);
+	if (!window)
+	{
+		return false;
+	}
+
+
+	
+	
+	
+
+	return true;
+}
--- VulkanGraphicsEngine/VulkanGraphicsEngine.h
+++ VulkanGraphicsEngine/VulkanGraphicsEngine.h
@@ -4,10 +4,90 @@
 #include <vulkan/vulkan_core.h>
 #include <vector>
 #include <memory>
+struct FrameCommandBuffer {
+	VkCommandBuffer commandBuffer;
+	VkFence fence;
+};
 class VulkanGraphicsEngine
 {
+public:
+	~VulkanGraphicsEngine();
+
+	//初期化
+	//ウインドウハンドル
+	//フレームバッファの幅
+	//フレームバッファの高さ
+	bool Init(int width, int height);
+	//1フレームのレンダリングの開始時に呼び出す
+	void BeginRender();
+	//1フレームのレンダリングの終了時に呼び出す
+	void EndRender();
+
+	VkInstance GetInstance()const
+	{
+		return m_instance;
+	}
+
+	VkPhysicalDevice GetPhySicalDevice()const
+	{
+		return m_physicalDevice;
+	}
 
 
+	VkDevice GetDevice()const
+	{
+		return m_device;
+	}
+
+	unsigned int GetBackBufferIndex()const
+	{
+		return m_frameIndex;
+	}
+
+
+	//コマンドキューを取得
+	VkQueue GetQueue()const
+	{
+		return m_commandQueue;
+	}
+
+	//コマンドバッファを取得
+	std::vector<FrameCommandBuffer> GetCommandBuffers()
+	{
+		return m_commandBuffers;
+	}
+
+	//TODO:定数とシェーダーのディスクリプタサイズについて調べておく
+
+	RenderingContext& GetRenderingContext()
+	{
+		return m_renderingcontext;
+	}
+
+
+	unsigned int GetFrameBufferWidth()const
+	{
+		return m_frameBufferWidth;
+	}
+
+	unsigned int GetFrameBufferHeight()const
+	{
+		return m_frameBufferHeight;
+	}
+
+	void ChangeRenderTargetToFrameBuffer(RenderingContext& rc);
+
+	void CopyToFrameBuffer(RenderingContext& rc, VkBuffer vkbuffer)
+	{
+//rc.copyresource
+		
+	}
+
+	//ヌルテクスチャマップを取得
+	const NullTextureMaps& GetNullTextureMaps() const
+	{
+		return m_nullTextureMaps;
+	}
 private:
 
 	//GPUベンダー定義
@@ -32,10 +112,7 @@ private:
 	VkExtent2D      m_surfaceExtent;
 	VkSwapchainKHR  m_swapchain = VK_NULL_HANDLE;
 	std::vector<vk::ImageResource> m_renderTargets;
-	struct FrameCommandBuffer {
-		VkCommandBuffer commandBuffer;
-		VkFence fence;
-	};
+	
 	std::vector<FrameCommandBuffer> m_commandBuffers;
 
 	VkDebugReportCallbackEXT  m_debugReport = VK_NULL_HANDLE;
@@ -67,9 +144,11 @@ private:
 	NullTextureMaps m_nullTextureMaps;			//ヌルテクスチャマップ。
 
 	Camera m_camera2D;							//2Dカメラ。
-	Camera m_camera3D;							//3Dカメラ。
+	Camera m_camera3D;
+	
+	//3Dカメラ。
 };
 
-extern std::shared_ptr<VulkanGraphicsEngine> g_graphicengine;
+extern VulkanGraphicsEngine* g_graphicengine;
 extern std::shared_ptr < Camera> g_camera2D;					//2Dカメラ。
 extern std::shared_ptr < Camera> g_camera3D;					//3Dカメラ。
