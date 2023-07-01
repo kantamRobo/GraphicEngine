324bd3c18f531e62af6170fe040f19605faf35a6
anazi6j <hattendosummer@yahoo.co.jp>
2023-07-02 07:00:33 +0900

    微修正

--- VulkanGraphicsEngine/RenderingContext.h
+++ VulkanGraphicsEngine/RenderingContext.h
@@ -2,23 +2,25 @@
 #include "UniformBuffer.h"
 #include "VulkanTexture.h"
 #include "FrameBuffer.h"
+#include "VulkanVertexBuffer.h"
+#include "VulkanIndexBuffer.h"
 class RenderingContext
 {//2023.6.7目標
 //４つ実装
 	void InitRenderingContext(std::shared_ptr<VkCommandBuffer> commandbuffer)
 	{
-		m_commandBuffer = commandbuffer;
+		m_commandBuffer = *commandbuffer;
 	}
 
 	//頂点バッファを設定
-	void SetVertexBuffer(std::shared_ptr<VertexBuffer> in_vb)
+	void SetVertexBuffer(std::shared_ptr<VulkanVertexBuffer> in_vb)
 	{
 		//TODO
 		vkCmdBindVertexBuffers(m_commandBuffer, );
 	}
 
 	//インデックスバッファを設定
-	void SetIndexBuffer(std::shared_ptr<Indexbuffer> ib)
+	void SetIndexBuffer(std::shared_ptr<VulkanIndexbuffer> ib)
 	{
 		//TODO
 		vkCmdBindIndexBuffer(m_commandBuffer);
