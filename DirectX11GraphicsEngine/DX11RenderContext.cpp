ee11d99c34aa2e0d79f2a3ee00c44bf05a7aadd8
Kanta yukawa <hattendosummer@yahoo.co.jp>
2024-03-15 14:11:32 +0900

    DX11レンダーコンテキストの定義を固めた ・パイプラインの制御にかかわる関数群にすることに。 関連する関数をほぼすべて実装した。残りはインダイレクト描画のみ

--- DirectX11GraphicsEngine/DX11RenderContext.cpp
+++ DirectX11GraphicsEngine/DX11RenderContext.cpp
@@ -1,5 +1,8 @@
 #include "DX11RenderContext.h"
 
+//セッタ
+
+//入力アセンブリ
 void DX11RenderContext::SetVertexBuffer(ID3D11Buffer* vertexbuffers)
 {
 	
@@ -12,55 +15,143 @@ void DX11RenderContext::SetIndexBuffer(ID3D11Buffer* indexbuffer)
 	m_rendercontext->IASetIndexBuffer(indexbuffer, DXGI_FORMAT_R8_UINT, 0);//暫定的な処置
 }
 
-void DX11RenderContext::SetViewPorts(UINT numviewports,const D3D11_VIEWPORT* viewports)
+
+	//ATTENTION:シェーダーに関しては、初期化に関してはShaderクラスを作成し、そこでシェーダーの作成を行う。ここではグラフィックスパイプラインを動かすコンテキストの一部
+	//として、シェーダーのセット関数をこちらに実装している。
+	void DX11RenderContext::SetVertexShader(ID3D11VertexShader * vertexShader, UINT numclassInstances)
+	{
+		m_rendercontext->VSSetShader(vertexShader,nullptr,numclassInstances);
+	}
+
+
+	void DX11RenderContext::SetComputeShader(ID3D11ComputeShader * computeShader, UINT numclassInstances)
+	{
+
+		m_rendercontext->CSSetShader(computeShader, nullptr, numclassInstances);
+	}
+
+	void DX11RenderContext::SetHullShader(ID3D11HullShader * hullShader, UINT numclassInstances)
+	{
+		m_rendercontext->HSSetShader(hullShader, nullptr, numclassInstances);
+	}
+
+	void DX11RenderContext::SetDomainShader(ID3D11DomainShader * DomainShader, UINT numclassInstances)
+	{
+		m_rendercontext->DSSetShader(DomainShader, nullptr, numclassInstances);
+	}
+
+
+//ラスタライザステージ
+
+	void DX11RenderContext::SetRasterizerState(ID3D11RasterizerState * rasterizerstate)
+	{
+		m_rendercontext->RSSetState(rasterizerstate);
+	}
+
+
+	void DX11RenderContext::SetViewPorts(UINT numviewports, const D3D11_VIEWPORT * viewports)
+	{
+		m_rendercontext->RSSetViewports(numviewports, viewports);
+
+
+		
+	}
+
+	void  DX11RenderContext::SetSingleViewPort(const D3D11_VIEWPORT* viewport)
+	{
+
+		m_rendercontext->RSSetViewports(2, viewport);
+
+		//エラー処理入れる?(複数のビューポートが入ってるぞとか)
+	}
+
+
+void DX11RenderContext::SetScssciorRects(UINT numRects, D3D11_RECT* rects)
 {
-	m_rendercontext->RSSetViewports(numviewports,viewports);
+	m_rendercontext->RSSetScissorRects(numRects, rects);
 }
 
-void DX11RenderContext::SetRenderTargets(UINT numviews, ID3D11RenderTargetView* rendertargetviews,ID3D11DepthStencilView* depthstencilviews)
-{
-	m_rendercontext->OMSetRenderTargets(numviews,&rendertargetviews,depthstencilviews);
-}
-
-void DX11RenderContext::SetVertexShader(ID3D11VertexShader* vertexShader, UINT numclassInstances)
-{
-	m_rendercontext->VSSetShader(vertexShader,nullptr,numclassInstances);
-}
+//ピクセルシェーダーステージ
 
 void DX11RenderContext::SetPixelShader(ID3D11PixelShader* pixelShader, UINT numclassInstances)
 {
-	m_rendercontext->PSSetShader(pixelShader,nullptr,numclassInstances);
+	m_rendercontext->PSSetShader(pixelShader, nullptr, numclassInstances);
 
 }
 
-void DX11RenderContext::SetComputeShader(ID3D11ComputeShader* computeShader, UINT numclassInstances)
-{
-	
-	m_rendercontext->CSSetShader(computeShader, nullptr, numclassInstances);
-}
-
-void DX11RenderContext::SetHullShader(ID3D11HullShader* hullShader, UINT numclassInstances)
-{
-	m_rendercontext->HSSetShader(hullShader, nullptr, numclassInstances);
-}
-
-void DX11RenderContext::SetDomainShader(ID3D11DomainShader* DomainShader, UINT numclassInstances)
-{
-	m_rendercontext->DSSetShader(DomainShader, nullptr, numclassInstances);
-}
-
-void DX11RenderContext::DrawIndexed(UINT indexCount)
-{
-	m_rendercontext->DrawIndexed(indexCount,0,0);
-	
-}
-
+//命令
 void DX11RenderContext::DrawIndexedInstanced(UINT indexcountperinstance,UINT instanceCount)
 {
 	m_rendercontext->DrawIndexedInstanced(indexcountperinstance,instanceCount,0,0,0);
 }
-
+//Indirectってなんぞや
+//https://sites.google.com/site/monshonosuana/directx%E3%81%AE%E8%A9%B1/directx%E3%81%AE%E8%A9%B1-%E7%AC%AC165%E5%9B%9E
+//Indirect命令
 void DX11RenderContext::DrawIndexedInstancedIndirect(ID3D11Buffer* args,UINT AlingedByteOffset) {
 	m_rendercontext->DrawIndexedInstancedIndirect(args,AlingedByteOffset);
+	
+}
+void DX11RenderContext::DrawDispatchIndirect(ID3D11Buffer* GPUPrimitivebuffer,UINT Offset)
+{
+	m_rendercontext->DispatchIndirect(GPUPrimitivebuffer, Offset);
 
 }
+
+void DX11RenderContext::DrawIndexed(UINT indexCount)
+{
+	m_rendercontext->DrawIndexed(indexCount, 0, 0);
+
+}
+
+
+
+void DX11RenderContext::SetRenderTargets(UINT numviews, ID3D11RenderTargetView* rendertargetviews, ID3D11DepthStencilView* depthstencilviews)
+{
+	m_rendercontext->OMSetRenderTargets(numviews, &rendertargetviews, depthstencilviews);
+}
+
+void DX11RenderContext::SetBlendState(ID3D11BlendState* blenderstate)
+{
+	m_rendercontext->OMSetBlendState(blenderstate, nullptr, 0xffffffff);
+}
+
+
+
+void DX11RenderContext::SetSingleRenderTarget(ID3D11RenderTargetView* rendertargetviews, ID3D11DepthStencilView* depthstencilviews)
+{
+	m_rendercontext->OMSetRenderTargets(1, &rendertargetviews, depthstencilviews);
+	//エラー処理入れる?(複数のレンダーターゲットが入ってるぞとか)
+}
+//画面クリア
+void DX11RenderContext::ClearRenderTargetView(ID3D11RenderTargetView* RenderTargetView,float nextcolor[4])
+{
+	m_rendercontext->ClearRenderTargetView(RenderTargetView, nextcolor);
+}
+
+//ストリーム出力マージャー(グラフィックパイプラインからコンピュートパイプラインに入力する)
+//https://learn.microsoft.com/ja-jp/windows/win32/direct3d12/stream-output-counters
+//基本的に頂点をバッファーに書き込む機能に使う
+void DX11RenderContext::SetStates(UINT numbuffers,ID3D11Buffer* vertexbuffer,UINT offset)
+{
+	m_rendercontext->SOSetTargets(numbuffers, &vertexbuffer, &offset);
+}
+
+void DX11RenderContext::SetState(ID3D11Buffer* vertexbufferforCompute)
+{
+	m_rendercontext->SOSetTargets(1,&vertexbufferforCompute,0);
+
+	//エラー処理入れる?(複数のバッファが入ってるぞとか)
+}
+
+
+
+
+//コンピュートパイプライン:こちらはグラフィックスパイプラインとはつながりはあるが、流れの中には組み込まれていないパイプラインを指す。
+void DX11RenderContext::Dispatch(UINT numthreadX, UINT numthreadY, UINT numthreadZ)
+{
+
+	m_rendercontext->Dispatch(numthreadX, numthreadY, numthreadZ);
+}
+
+
+//ゲッタ
--- DirectX11GraphicsEngine/DX11RenderContext.h
+++ DirectX11GraphicsEngine/DX11RenderContext.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <d3d11.h>
 #include <wrl.h>
+
+//グラフィックスパイプラインに関わる関数を一元管理する(入力アセンブリ・シェーダー・ラスタライザ・アウトプットマージャー,またはコンピュートパイプラインの実行や、それにかかわるコマンド)
 class DX11RenderContext
 {
 public:
@@ -24,8 +26,32 @@ public:
 	void SetIndexBuffer(ID3D11Buffer* indexbuffer);
 
 	void SetViewPorts(UINT numviewports, const D3D11_VIEWPORT* viewports);
+	void SetSingleViewPort(const D3D11_VIEWPORT* viewports);
+	void SetScssciorRects(UINT numRects, D3D11_RECT* rects);
+
+	
+
+	
 
 	void SetRenderTargets(UINT numviews, ID3D11RenderTargetView* rendertargetviews, ID3D11DepthStencilView* depthstencilviews);
+
+	void SetBlendState(ID3D11BlendState* blenderstate);
+
+	
+
+	
+
+	void SetSingleRenderTarget(ID3D11RenderTargetView* rendertargetviews, ID3D11DepthStencilView* depthstencilviews);
+
+	void ClearRenderTargetView(ID3D11RenderTargetView* RenderTargetView, float nextcolor[4]);
+
+	
+
+	void SetStates(UINT numbuffers, ID3D11Buffer* vertexbuffer, UINT offset);
+
+	void SetState(ID3D11Buffer* vertexbuffer);
+
+	
 
 	
 	
@@ -44,16 +70,26 @@ public:
 
 	void SetDomainShader(ID3D11DomainShader* DomainShader, UINT numclassInstances);
 
+	void SetRasterizerState(ID3D11RasterizerState* rasterizerstate);
+
+	void SetViewPort(const D3D11_VIEWPORT* viewport);
+
+	
 	void DrawIndexed(UINT indexCount);
 
 	void DrawIndexedInstanced(UINT indexcountperinstance, UINT instanceCount);
 
-	void DrawIndexedInstancedIndirect(ID3D11Buffer* args, UINT AlingedByteOffset);
+	void DrawIndexedInstancedIndirect(ID3D11Buffer* GPUPrimitiveBuffer, UINT AlingedByteOffset);
+
+	void DrawDispatchIndirect(ID3D11Buffer* GPUPrimitivebuffer, UINT Offset);
 
 	
 	
-
 	
+
+	//Compute Pipeline
+
+	void Dispatch(UINT numthreadX, UINT numthreadY, UINT numthreadZ);
 
 	
 	
