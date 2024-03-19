e353b6305b0eae7f1026b800ea357f18efc518aa
Kanta_Yukawa <36531957+kantamRobo@users.noreply.github.com>
2024-03-19 11:26:17 +0900

    微修正

--- DirectX11GraphicsEngine/Renderer.cpp
+++ DirectX11GraphicsEngine/Renderer.cpp
@@ -4,7 +4,7 @@ void Renderer::Init(HWND hwnd, const Microsoft::glTF::Document& doc, const std::
 {
 	graphicEngine.CreateDevice();
 	graphicEngine.CreateSwapChain(hwnd);
-	graphicEngine.CreateRTV();
+	graphicEngine.CreateRTV(640,480);
 	context = std::make_shared<DX11RenderContext>(graphicEngine.m_deviceContext);
 	model.Init(graphicEngine.m_device.Get(), doc, attrName);
 }
